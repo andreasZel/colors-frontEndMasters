c9130729e0adf9860ad1648c675b9769816d6b75
Andreas Zelios <a.zelios@satways.net>
2025-02-10 20:38:35 +0200

    created color reducer with types

---
+++ src/lib/colorReducer.ts
@@ -0,0 +1,40 @@
+import { rgb } from 'color-convert';
+
+type UpdateHexColorAction = {
+    type: 'update-hex-color';
+    payload: {
+        hexColor: string;
+    };
+};
+
+type UpdateRGBColorAction = {
+    type: 'update-rgb-color';
+    payload: {
+        rgb: [number, number, number];
+    };
+};
+
+type ColorState = {
+    hexColor: string;
+}
+
+export const InitialState: ColorState = {
+    hexColor: '#BADA55'
+};
+
+export const colorReducer = (state: ColorState = InitialState, action: UpdateHexColorAction | UpdateRGBColorAction) => {
+
+    if (action.type === 'update-hex-color') {
+        const { hexColor } = action.payload;
+
+        return { ...state, hexColor };
+    }
+
+    if (action.type === 'update-rgb-color') {
+        const hexColor = rgb.hex(action.payload.rgb);
+
+        return { ...state, hexColor };
+    }
+
+    return state;
+}
